8d4fc81b62d411c15c4625d49bc29b8dfed90ee7
Cycatz <thesummernightcat@gmail.com>
2019-11-13 19:03:31 +0800

    add requestClient.py

---
+++ requestClient.py
@@ -0,0 +1,31 @@
+import requests
+
+session = None
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML,like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+}
+
+def Initialize():
+    global session
+    session = requests.Session()
+
+def post(url, data, headers=headers):
+    success = False
+    try:
+        global session
+        res = session.post(url, headers=headers, data=data)
+        res.raise_for_status()
+        success = True
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    finally:
+        return success
+
+def get(url):
+    global session
+    res = session.get(url)
+    return res
+
+
