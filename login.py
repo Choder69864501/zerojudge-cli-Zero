562d3ee545feca94998465dabd64f969b0c99916
Cycatz <thesummernightcat@gmail.com>
2019-11-13 14:23:05 +0800

    edit login.py

--- login.py
+++ login.py
@@ -12,17 +12,14 @@ user['g-recaptcha'] = input('Recaptcha')
 
 
 headers = {
-    'Content-Type':'application/x-www-form-urlencoded',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML,like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 }
 
 session = requests.Session()
 session.post(loginUrl, headers=headers, data=user)
-#res = session.get('https://zerojudge.tw/Submissions?account=Cycatz')
 
-data={'action':'SubmitCode','language':'CPP','code':'vasv','contestid':'0','problemid':'a001'}
 session.post('https://zerojudge.tw/Solution.api',headers=headers,data=data)
-# soup = bs(res.text, 'html.parser')
-# panel = soup.select('.panel')
 
     
 
