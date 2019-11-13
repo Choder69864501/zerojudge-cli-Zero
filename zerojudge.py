16a6315b52b7ab606ead37d367c5767faabc8e3d
Cycatz <thesummernightcat@gmail.com>
2019-11-13 19:05:52 +0800

    implement login function

--- zerojudge.py
+++ zerojudge.py
@@ -1,28 +1,41 @@
+import getpass
 from bs4 import BeautifulSoup as bs
-import requests
+from cmd import Cmd
+
+import requestClient 
 
 
-user = {}
-session = ''
+user = None
 
 
 def Login():
 
-    loginUrl = 'https://zerojudge.tw/Login'
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML,like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    }
+    global user
 
-    session = requests.Session()
-    res = session.post(loginUrl, headers=headers, data=user)
-    print(res.text)
+    if user is not None:
+        print('You are already logged!!')
+    else:
+        username = input('Please input your username: ') 
+        password = getpass.getpass('Please input your password: ') 
+        recaptchaResponse = input('Please input the recaptcha response, you can use Shift+Insert to paste: ') 
 
+        loginUrl = 'https://zerojudge.tw/Login'
 
-def Submission():
+        user = {
+            'account': username,
+            'passwd': password,
+            'g-recaptcha-response': recaptchaResponse,
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML,like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        }
+        requestClient.Initialize()
+        if requestClient.post(loginUrl, user, headers):
+            print('Login success!')
 
+def Submit():
     submitUrl = 'https://zerojudge.tw/Solution.api'
 
 
-    
 
